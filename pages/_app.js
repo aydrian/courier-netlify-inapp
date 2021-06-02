1e64142f822b59da845e7c3de271775bae6f8ee7
Aydrian Howard <aydrian@gmail.com>
2021-06-02 10:13:02 -0400

    initial toast component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+import { useState, useEffect } from "react";
+import { CourierProvider } from "@trycourier/react-provider";
+import { Toast } from "@trycourier/react-toast";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [courierProps, setCourierProps] = useState({});
+  useEffect(() => {
+    setCourierProps({
+      clientKey: "MjQwOTUyMDMtMGJiOS00ZDY1LWFhOGEtODRjMDUyNGI4MWUy",
+      userId: "AYDRIAN10036"
+    });
+  }, []);
+  return (
+    <CourierProvider {...courierProps}>
+      <Toast />
+      <Component {...pageProps} />
+    </CourierProvider>
+  );
 }
 
-export default Application
+export default Application;
